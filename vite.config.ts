82f86ce6239829e741f86aae4592bb17d7669a63
Shantanu <shantanupathak29@gmail.com>
2026-07-22 18:55:36 +0530

    Changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,16 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
     tanstackStart({
       server: { entry: "server" },
     }),
     viteReact(),
     tailwindcss(),
-    tsConfigPaths(),
   ],
 });
